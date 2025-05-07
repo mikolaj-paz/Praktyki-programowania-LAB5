2d5ec7a818aba5526b147e8feb4e0a8d62f91d83
Mikołaj Paździor <mikolajpaz@gmail.com>
2025-05-07 16:03:27 +0200

    Add conversion functionality

--- utils.py
+++ utils.py
@@ -21,3 +21,15 @@ def multiply(a: int, b: int) -> int:
 def divide(a: int, b: int) -> float:
     """Divides one integer by another. Returns float"""
     return a / b
+
+def to_binary(a) -> int:
+    """Converts a number to binary."""
+    if (not isinstance(a, int) or not 0 <= a <= 100):
+        return -1
+    
+    res = ''
+    while a > 0:
+        res = str(a & 1) + res
+        a >>= 1
+    
+    return int(res)
